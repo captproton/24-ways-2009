8057d95f90ce7a3194fa409f7270638b24caebd2
Carl Tanner <carl@captproton.com>
2010-07-05 22:34:53 -0700

    added working photos show page for links from home index

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,6 +3,18 @@ class PhotosController < ApplicationController
   end
 
   def show
+    if params[:photo_id]
+      # params are given by the home index
+      @photo_url = 'http://farm'+ params[:farm] + '.static.flickr.com/' + params[:server]         + '/'         + params[:photo_id]         + '_'         + params[:secret]         + '_z_d.jpg'
+      
+      # @source = URI.unescape(source_param)
+    else
+      @photo_url = "http://flickr.com"
+    end
+    
+    flickrQuery = 'select farm,id,owner,secret,server,title from flickr.photos.search where user_id="11414938@N00"'
+    
+    @photo = ""
   end
 
 end
