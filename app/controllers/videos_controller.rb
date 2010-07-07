73dfbfd0af321b3393141d15c51233d281172197
Carl Tanner <carl@captproton.com>
2010-07-07 13:54:50 -0700

    video now loads and plays after page loads

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,7 +8,9 @@ class VideosController < ApplicationController
       video_param = params[:videoid]
       @videoid = URI.unescape(video_param)
       youtube_query = 'select * from feed(5) where url="http://gdata.youtube.com/feeds/api/users/chrisheilmann/uploads?v=2" and group.videoid="'+ @videoid +'"'
+      youtube_stream = 'select * from feed(5) where url="http://gdata.youtube.com/feeds/api/users/chrisheilmann/uploads?v=2" and group.videoid="'+ @videoid +'"'
       @video = yql_search(youtube_query).first.second
+      @videos = yql_search(youtube_stream).first.second
 
     else
       @source = "/"
