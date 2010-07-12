6e7b222c49eaebb1745b090059b8a20634eff805
Carl Tanner <carl@captproton.com>
2010-07-11 18:20:46 -0700

    attempt to create yql-based feeds; one feed per show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :feeds
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   map.resource  :blogs,       :controller => 'blogs', :only => [:index, :show] 
